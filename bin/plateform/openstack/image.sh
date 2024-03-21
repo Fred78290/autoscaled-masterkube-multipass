25bb0df33206a748d20494368625add78b415682
Fred78290 <frederic.boltz@gmail.com>
2024-03-21 10:45:17 +0100

    DRAFT: Add openstack support

---
+++ bin/plateform/openstack/image.sh
@@ -0,0 +1,210 @@
+#!/bin/bash
+
+set -e
+
+# This script will create a VM used as template
+# This step is done by importing https://cloud-images.ubuntu.com/${DISTRO}/current/${DISTRO}-server-cloudimg-amd64.img
+# This VM will be used to create the kubernetes template VM 
+
+PRIMARY_NETWORK_NAME="private"
+TARGET_IMAGE=
+FLAVOR_IMAGE=ds2G
+SECURITY_GROUP=default
+
+OPTIONS=(
+	"distribution:"
+	"custom-image:"
+	"ssh-key:"
+	"ssh-priv-key:"
+	"cni-version:"
+	"password:"
+	"seed:"
+	"arch:"
+	"user:"
+	"kubernetes-version:"
+	"primary-network:"
+	"second-network:"
+	"k8s-distribution:"
+	"container-runtime:"
+	"primary-adapter:"
+	"flavor:"
+	"security-group:"
+)
+
+PARAMS=$(echo ${OPTIONS[*]} | tr ' ' ',')
+TEMP=$(getopt -o d:i:k:n:p:s:a:u:v:o --long "${PARAMS}"  -n "$0" -- "$@")
+
+eval set -- "${TEMP}"
+
+# extract options and their arguments into variables.
+while true ; do
+	#echo "1:$1"
+	case "$1" in
+		-d|--distribution)
+			DISTRO="$2"
+			SEED_IMAGE=${DISTRO}-server-cloudimg-seed
+			shift 2
+			;;
+		-i|--custom-image) TARGET_IMAGE="$2" ; shift 2;;
+		-k|--ssh-key) SSH_KEY=$2 ; shift 2;;
+		--ssh-priv-key) SSH_PRIVATE_KEY=$2 ; shift 2;;
+		-n|--cni-version) CNI_VERSION=$2 ; shift 2;;
+		-p|--password) KUBERNETES_PASSWORD=$2 ; shift 2;;
+		-s|--seed) SEED_IMAGE=$2 ; shift 2;;
+		-a|--arch) SEED_ARCH=$2 ; shift 2;;
+		-u|--user) KUBERNETES_USER=$2 ; shift 2;;
+		-v|--kubernetes-version) KUBERNETES_VERSION=$2 ; shift 2;;
+		--primary-network) PRIMARY_NETWORK_NAME=$2 ; shift 2;;
+		--flavor) FLAVOR_IMAGE=$2 ; shift 2;;
+		--security-group) SECURITY_GROUP=$2 ; shift 2;;
+		--k8s-distribution) 
+			case "$2" in
+				kubeadm|k3s|rke2|microk8s)
+				KUBERNETES_DISTRO=$2
+				;;
+			*)
+				echo "Unsupported kubernetes distribution: $2"
+				exit 1
+				;;
+			esac
+			shift 2
+			;;
+		--container-runtime)
+			case "$2" in
+				"docker")
+					CONTAINER_ENGINE="$2"
+					CONTAINER_CTL=docker
+					;;
+				"cri-o"|"containerd")
+					CONTAINER_ENGINE="$2"
+					CONTAINER_CTL=crictl
+					;;
+				*)
+					echo_red_bold "Unsupported container runtime: $2"
+					exit 1
+					;;
+			esac
+			shift 2;;
+		--) shift ; break ;;
+		*) echo_red_bold "$1 - Internal error!" ; exit 1 ;;
+	esac
+done
+
+if [ -z "${TARGET_IMAGE}" ]; then
+	TARGET_IMAGE=${DISTRO}-${KUBERNETES_DISTRO}-${KUBERNETES_VERSION}-${SEED_ARCH}
+fi
+
+TARGET_IMAGE_ID=$(openstack image list --all -f json | jq -r --arg TARGET_IMAGE ${TARGET_IMAGE} '.[]|select(.Name == $TARGET_IMAGE)|.ID')
+
+if [ -n "${TARGET_IMAGE_ID}" ]; then
+	echo_blue_bold "${TARGET_IMAGE} already exists!"
+	exit 0
+fi
+
+echo_blue_bold "Ubuntu password:${KUBERNETES_PASSWORD}"
+
+NETWORK_ID=$(openstack network list --name ${PRIMARY_NETWORK_NAME} -f json | jq -r '.[]|.ID // ""')
+SEED_IMAGE=${DISTRO}-server-cloudimg-${SEED_ARCH}
+SEED_IMAGE_URL="https://cloud-images.ubuntu.com/${DISTRO}/current/${SEED_IMAGE}.img"
+SEED_IMAGE_ID=$(openstack image list --all -f json | jq -r --arg SEED_IMAGE ${SEED_IMAGE} '.[]|select(.Name == $SEED_IMAGE)|.ID')
+
+if [ -z "${NETWORK_ID}" ]; then
+	echo_bold_red "Network ${PRIMARY_NETWORK_NAME}, not found"
+	exit 1
+fi
+
+if [ -z "${SEED_IMAGE_ID}" ]; then
+	[ -f ${CACHE}/${SEED_IMAGE}.img ] || curl -Ls ${SEED_IMAGE_URL} -o ${CACHE}/${SEED_IMAGE}.img
+
+	echo_blue_bold "Import file ${CACHE}/${SEED_IMAGE}.img to image named: ${SEED_IMAGE}"
+
+	SEED_IMAGE_ID=$(openstack image create --container-format bare --disk-format qcow2 --private --file ${CACHE}/${SEED_IMAGE}.img -f json ${SEED_IMAGE} | jq -r '.id//""')
+
+	if [ -z "${SEED_IMAGE_ID}" ]; then
+		echo_bold_red "Import failed"
+		exit 1
+	fi
+fi
+
+
+mkdir -p ${CACHE}/packer/cloud-data
+
+cat > ${CACHE}/packer/cloud-data/user-data <<EOF
+#cloud-config
+timezone: ${TZ}
+package_update: false
+package_upgrade: false
+ssh_pwauth: true
+write_files:
+- encoding: gzip+base64
+  content: $(cat ${CURDIR}/prepare-image.sh | gzip -c9 | base64 -w 0)
+  owner: root:adm
+  path: /usr/local/bin/prepare-image.sh
+  permissions: '0755'
+users:
+  - name: ${KUBERNETES_USER}
+    groups: users, admin
+    lock_passwd: false
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    shell: /bin/bash
+    plain_text_passwd: ${KUBERNETES_PASSWORD}
+    ssh_authorized_keys:
+      - ${SSH_KEY}
+  - name: packer
+    sudo: ALL=(ALL) NOPASSWD:ALL
+    groups: users, admin
+    lock_passwd: false
+    plain_text_passwd: packerpassword
+    ssh_authorized_keys:
+      - ${SSH_KEY}
+apt:
+  preserve_sources_list: true
+EOF
+
+# 	"external_source_image_url": "${SEED_IMAGE_URL}",
+
+OPENSTACK_BUILDER=$(cat <<EOF
+{
+	"type": "openstack",
+	"identity_endpoint": "${OS_AUTH_URL}",
+	"domain_name": "${OS_USER_DOMAIN_NAME}",
+	"tenant_id": "${OS_PROJECT_ID}",
+	"tenant_name": "${OS_PROJECT_NAME}",
+	"username": "${OS_USERNAME}",
+	"password": "${OS_PASSWORD}",
+	"region": "${OS_REGION_NAME}",
+	"ssh_username": "packer",
+	"ssh_password": "packerpassword",
+	"ssh_handshake_attempts": 10,
+	"image_name": "${TARGET_IMAGE}",
+	"source_image": "${SEED_IMAGE_ID}",
+	"flavor": "${FLAVOR_IMAGE}",
+	"insecure": "true",
+	"ssh_private_key_file": "${SSH_PRIVATE_KEY}",
+	"floating_ip_network": "${VC_NETWORK_PUBLIC}",
+	"image_visibility": "private",
+	"external_source_image_format": "qcow2",
+	"security_groups": "${SECURITY_GROUP}",
+	"networks": [ "${NETWORK_ID}" ],
+	"user_data_file": "${CACHE}/packer/cloud-data/user-data"
+}
+EOF
+)
+
+cat ./templates/packer/template.json | jq --argjson OPENSTACK_BUILDER "${OPENSTACK_BUILDER}" '.builders[0] = $OPENSTACK_BUILDER' > ${CACHE}/packer/template.json
+
+KUBERNETES_MINOR_RELEASE=$(echo -n ${KUBERNETES_VERSION} | tr '.' ' ' | awk '{ print $2 }')
+CRIO_VERSION=$(echo -n ${KUBERNETES_VERSION} | tr -d 'v' | tr '.' ' ' | awk '{ print $1"."$2 }')
+
+echo_blue_bold "Prepare ${TARGET_IMAGE} image with cri-o version: ${CRIO_VERSION} and kubernetes: ${KUBERNETES_VERSION}"
+
+INIT_SCRIPT="/usr/local/bin/prepare-image.sh --container-runtime ${CONTAINER_ENGINE} --cni-version ${CNI_VERSION} --cni-plugin ${CNI_PLUGIN} --kubernetes-version ${KUBERNETES_VERSION} --k8s-distribution ${KUBERNETES_DISTRO}"
+
+pushd ${CACHE}/packer/
+export PACKER_LOG=1
+packer build -var INIT_SCRIPT="${INIT_SCRIPT}" template.json
+popd
+
+echo_blue_bold "Created image ${TARGET_IMAGE} with kubernetes version ${KUBERNETES_VERSION}"
+
+exit 0
---
+++ bin/plateform/openstack/plateform.sh
@@ -0,0 +1,292 @@
+CMD_MANDATORIES="envsubst helm kubectl jq yq cfssl kubernetes-desktop-autoscaler-utility packer"
+NET_IF=eth1 # eth0 is multipass interface
+VC_NETWORK_PRIVATE="private"
+VC_NETWORK_PUBLIC="public"
+NGINX_MACHINE="k8s.tiny"
+CONTROL_PLANE_MACHINE="k8s.small"
+WORKER_NODE_MACHINE="k8s.medium"
+AUTOSCALE_MACHINE="k8s.medium"
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function use_floating_ip() {
+	local INDEX=$1
+
+	if [ ${HA_CLUSTER} = "true" ]; then
+		if [ ${INDEX} -lt ${CONTROLNODE_INDEX} ]; then
+			echo -n ${EXPOSE_PUBLIC_CLUSTER}
+		elif [ ${INDEX} -lt ${WORKERNODE_INDEX} ]; then
+			echo -n ${CONTROLPLANE_USE_PUBLICIP}
+		else
+			echo -n ${WORKERNODE_USE_PUBLICIP}
+		fi
+	elif [ ${INDEX} -lt ${WORKERNODE_INDEX} ]; then
+		echo -n ${CONTROLPLANE_USE_PUBLICIP}
+	else
+		echo -n ${WORKERNODE_USE_PUBLICIP}
+	fi
+
+	echo -n "NO"
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function create_vm() {
+	local INDEX=$1
+	local NODE_IP=$2
+	local SECURITY_GROUP=$3
+	local FLOATING_IP=$(use_floating_ip ${INDEX})
+	local MACHINE_TYPE=
+	local MASTERKUBE_NODE=
+	local IPADDR=
+	local VMHOST=
+	local DISK_SIZE=
+	local NUM_VCPUS=
+	local MEMSIZE=
+
+	MACHINE_TYPE=$(get_machine_type ${INDEX})
+	MASTERKUBE_NODE=$(get_vm_name ${INDEX})
+	MASTERKUBE_NODE_UUID=$(get_vmuuid ${MASTERKUBE_NODE})
+
+	if [ -z "${MASTERKUBE_NODE_UUID}" ]; then
+		NETWORK_ID=$(openstack network show -f json private | jq -r '.id//""')
+
+		# Cloud init user-data
+		cat > ${TARGET_CONFIG_LOCATION}/userdata-${INDEX}.yaml <<EOF
+#cloud-config
+package_update: ${UPDATE_PACKAGE}
+package_upgrade: ${UPDATE_PACKAGE}
+timezone: ${TZ}
+growpart:
+  mode: auto
+  devices: ["/"]
+  ignore_growroot_disabled: false
+EOF
+
+if [ -f "${TARGET_CONFIG_LOCATION}/credential.yaml" ]; then
+		cat >> ${TARGET_CONFIG_LOCATION}/userdata-${INDEX}.yaml <<EOF
+write_files:
+- encoding: gzip+base64
+  content: $(cat ${TARGET_CONFIG_LOCATION}/credential.yaml | gzip -c9 | base64 -w 0)
+  owner: root:root
+  path: ${IMAGE_CREDENTIALS_CONFIG}
+  permissions: '0644'
+runcmd:
+- hostnamectl set-hostname ${MASTERKUBE_NODE}
+EOF
+fi
+
+		echo_line
+		echo_blue_bold "Launch ${TARGET_IMAGE} to ${MASTERKUBE_NODE} TARGET_IMAGE=${TARGET_IMAGE} MASTERKUBE_NODE=${MASTERKUBE_NODE}"
+		echo_line
+
+		if [ ${FLOATING_IP} == "YES" ]; then
+			IPADDR=$(openstack floating ip list --tags ${MASTERKUBE_NODE} -f json 2>/dev/null | jq -r '.[0]."Floating IP Address"//""')
+
+			if [ -z "${IPADDR}" ]; then
+				echo_blue_bold "Create floating ip for ${MASTERKUBE_NODE} on network ${VC_NETWORK_PUBLIC}"
+
+				IPADDR=$(openstack floating ip create --tag ${MASTERKUBE_NODE} -f json ${VC_NETWORK_PUBLIC} | jq -r '.floating_ip_address // ""')
+			fi
+		fi
+
+		LOCALIP=$(openstack server create \
+			--flavor "${MACHINE_TYPE}" \
+			--image "${TARGET_IMAGE}" \
+			--nic net-id=${NETWORK_ID},v4-fixed-ip=${NODE_IP} \
+			--security-group "${SECURITY_GROUP}" \
+			--key-name "${SSH_KEYNAME}" \
+			--user-data ${TARGET_CONFIG_LOCATION}/userdata-${INDEX}.yaml \
+			--wait \
+			-f json ${MASTERKUBE_NODE} 2>/dev/null | jq -r --arg NETWORK ${VC_NETWORK_PRIVATE}  '.addresses|.[$NETWORK][0]')
+
+		if [ ${FLOATING_IP} == "YES" ]; then
+			openstack server add floating ip ${MASTERKUBE_NODE} ${IPADDR}
+		else
+			IPADDR=${LOCALIP}
+		fi
+
+		echo_title "Wait ssh ready on ${KUBERNETES_USER}@${IPADDR}"
+		wait_ssh_ready ${KUBERNETES_USER}@${IPADDR}
+
+		echo_title "Prepare ${MASTERKUBE_NODE} instance with IP:${IPADDR}"
+		eval scp ${SCP_OPTIONS} tools ${KUBERNETES_USER}@${IPADDR}:~ ${SILENT}
+		eval ssh ${SSH_OPTIONS} ${KUBERNETES_USER}@${IPADDR} mkdir -p /home/${KUBERNETES_USER}/cluster ${SILENT}
+		eval ssh ${SSH_OPTIONS} ${KUBERNETES_USER}@${IPADDR} sudo chown -R root:adm /home/${KUBERNETES_USER}/tools ${SILENT}
+		eval ssh ${SSH_OPTIONS} ${KUBERNETES_USER}@${IPADDR} sudo cp /home/${KUBERNETES_USER}/tools/* /usr/local/bin ${SILENT}
+
+		# Update /etc/hosts
+		delete_host "${MASTERKUBE_NODE}"
+		add_host ${NODE_IP} ${MASTERKUBE_NODE} ${MASTERKUBE_NODE}.${DOMAIN_NAME}
+	else
+		echo_title "Already running ${MASTERKUBE_NODE} instance"
+	fi
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function delete_vm_by_name() {
+    local VMNAME=$1
+
+    if [ "$(openstack server show "${VMNAME}" 2>/dev/null)" ]; then
+        echo_blue_bold "Delete VM: ${VMNAME}"
+		openstack server delete --force --wait ${VMNAME} #2>/dev/null
+	fi
+
+    delete_host "${VMNAME}"
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function unregister_dns() {
+    echo
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function update_build_env() {
+    set +u
+cat ${PLATEFORMDEFS} > ${TARGET_CONFIG_LOCATION}/buildenv
+
+cat > ${TARGET_CONFIG_LOCATION}/buildenv <<EOF
+export AUTOSCALE_MACHINE=${AUTOSCALE_MACHINE}
+export AUTOSCALER_DESKTOP_UTILITY_ADDR=${AUTOSCALER_DESKTOP_UTILITY_ADDR}
+export AUTOSCALER_DESKTOP_UTILITY_CACERT=${AUTOSCALER_DESKTOP_UTILITY_CACERT}
+export AUTOSCALER_DESKTOP_UTILITY_CERT=${AUTOSCALER_DESKTOP_UTILITY_CERT}
+export AUTOSCALER_DESKTOP_UTILITY_KEY=${AUTOSCALER_DESKTOP_UTILITY_CERT}
+export AUTOSCALER_DESKTOP_UTILITY_TLS=${AUTOSCALER_DESKTOP_UTILITY_TLS}
+export AWS_ACCESSKEY=${AWS_ACCESSKEY}
+export AWS_ROUTE53_ACCESSKEY=${AWS_ROUTE53_ACCESSKEY}
+export AWS_ROUTE53_PUBLIC_ZONE_ID=${AWS_ROUTE53_PUBLIC_ZONE_ID}
+export AWS_ROUTE53_SECRETKEY=${AWS_ROUTE53_SECRETKEY}
+export AWS_SECRETKEY=${AWS_SECRETKEY}
+export CERT_GODADDY_API_KEY=${CERT_GODADDY_API_KEY}
+export CERT_GODADDY_API_SECRET=${CERT_GODADDY_API_SECRET}
+export CERT_ZEROSSL_EAB_HMAC_SECRET=${CERT_ZEROSSL_EAB_HMAC_SECRET}
+export CERT_ZEROSSL_EAB_KID=${CERT_ZEROSSL_EAB_KID}
+export CLOUD_PROVIDER_CONFIG=${CLOUD_PROVIDER_CONFIG}
+export CLOUD_PROVIDER=${CLOUD_PROVIDER}
+export CNI_PLUGIN=${CNI_PLUGIN}
+export CNI_VERSION=${CNI_VERSION}
+export CONFIGURATION_LOCATION=${CONFIGURATION_LOCATION}
+export CONTAINER_ENGINE=${CONTAINER_ENGINE}
+export CONTROL_PLANE_MACHINE=${CONTROL_PLANE_MACHINE}
+export CONTROLNODES=${CONTROLNODES}
+export CORESTOTAL="${CORESTOTAL}"
+export DOMAIN_NAME=${DOMAIN_NAME}
+export EXTERNAL_ETCD=${EXTERNAL_ETCD}
+export FIRSTNODE=${FIRSTNODE}
+export GRPC_PROVIDER=${GRPC_PROVIDER}
+export HA_CLUSTER=${HA_CLUSTER}
+export KUBECONFIG=${KUBECONFIG}
+export KUBERNETES_DISTRO=${KUBERNETES_DISTRO}
+export KUBERNETES_PASSWORD=${KUBERNETES_PASSWORD}
+export KUBERNETES_USER=${KUBERNETES_USER}
+export KUBERNETES_VERSION=${KUBERNETES_VERSION}
+export LAUNCH_CA=${LAUNCH_CA}
+export MASTER_NODE_ALLOW_DEPLOYMENT=${MASTER_NODE_ALLOW_DEPLOYMENT}
+export MASTERKUBE="${MASTERKUBE}"
+export MAXAUTOPROVISIONNEDNODEGROUPCOUNT=${MAXAUTOPROVISIONNEDNODEGROUPCOUNT}
+export MAXNODES=${MAXNODES}
+export MAXTOTALNODES=${MAXTOTALNODES}
+export MEMORYTOTAL="${MEMORYTOTAL}"
+export METALLB_IP_RANGE=${METALLB_IP_RANGE}
+export MINNODES=${MINNODES}
+export NET_DNS=${NET_DNS}
+export NET_DOMAIN=${NET_DOMAIN}
+export NET_GATEWAY=${NET_GATEWAY}
+export NET_IF=${NET_IF}
+export NET_IP=${NET_IP}
+export NET_MASK_CIDR=${NET_MASK_CIDR}
+export NET_MASK=${NET_MASK}
+export NETWORK_PRIVATE_ROUTES=(${NETWORK_PRIVATE_ROUTES[@]})
+export NETWORK_PUBLIC_ROUTES=(${NETWORK_PUBLIC_ROUTES[@]})
+export NFS_SERVER_ADDRESS=${NFS_SERVER_ADDRESS}
+export NFS_SERVER_PATH=${NFS_SERVER_PATH}
+export NFS_STORAGE_CLASS=${NFS_STORAGE_CLASS}
+export NGINX_MACHINE=${NGINX_MACHINE}
+export NODEGROUP_NAME="${NODEGROUP_NAME}"
+export OSDISTRO=${OSDISTRO}
+export PLATEFORM="${PLATEFORM}"
+export PUBLIC_DOMAIN_NAME=${PUBLIC_DOMAIN_NAME}
+export PUBLIC_IP="${PUBLIC_IP}"
+export REGION=${REGION}
+export REGISTRY=${REGISTRY}
+export RESUME=${RESUME}
+export ROOT_IMG_NAME=${ROOT_IMG_NAME}
+export SCALEDNODES_DHCP=${SCALEDNODES_DHCP}
+export SCALEDOWNDELAYAFTERADD=${SCALEDOWNDELAYAFTERADD}
+export SCALEDOWNDELAYAFTERDELETE=${SCALEDOWNDELAYAFTERDELETE}
+export SCALEDOWNDELAYAFTERFAILURE=${SCALEDOWNDELAYAFTERFAILURE}
+export SCALEDOWNENABLED=${SCALEDOWNENABLED}
+export SCALEDOWNUNEEDEDTIME=${SCALEDOWNUNEEDEDTIME}
+export SCALEDOWNUNREADYTIME=${SCALEDOWNUNREADYTIME}
+export SEED_ARCH=${SEED_ARCH}
+export SEED_IMAGE="${SEED_IMAGE}"
+export SEED_USER=${SEED_USER}
+export SILENT="${SILENT}"
+export SSH_KEY_FNAME=${SSH_KEY_FNAME}
+export SSH_KEY="${SSH_KEY}"
+export SSH_PRIVATE_KEY=${SSH_PRIVATE_KEY}
+export SSH_PUBLIC_KEY=${SSH_PUBLIC_KEY}
+export SSL_LOCATION=${SSL_LOCATION}
+export TARGET_CLUSTER_LOCATION=${TARGET_CLUSTER_LOCATION}
+export TARGET_CONFIG_LOCATION=${TARGET_CONFIG_LOCATION}
+export TARGET_DEPLOY_LOCATION=${TARGET_DEPLOY_LOCATION}
+export TARGET_IMAGE=${TARGET_IMAGE}
+export TRANSPORT=${TRANSPORT}
+export UNREMOVABLENODERECHECKTIMEOUT=${UNREMOVABLENODERECHECKTIMEOUT}
+export UPGRADE_CLUSTER=${UPGRADE_CLUSTER}
+export USE_DHCP_ROUTES_PRIVATE=${USE_DHCP_ROUTES_PRIVATE}
+export USE_DHCP_ROUTES_PUBLIC=${USE_DHCP_ROUTES_PUBLIC}
+export USE_KEEPALIVED=${USE_KEEPALIVED}
+export USE_ZEROSSL=${USE_ZEROSSL}
+export VC_NETWORK_PRIVATE=${VC_NETWORK_PRIVATE}
+export VC_NETWORK_PUBLIC=${VC_NETWORK_PUBLIC}
+export WORKERNODES=${WORKERNODES}
+export ZONEID=${ZONEID}
+EOF
+    set -u
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function update_provider_config() {
+    PROVIDER_AUTOSCALER_CONFIG=$(cat ${TARGET_CONFIG_LOCATION}/provider.json)
+
+    echo -n ${PROVIDER_AUTOSCALER_CONFIG} | jq --arg TARGET_IMAGE "${TARGET_IMAGE}" '.template-name = $TARGET_IMAGE' > ${TARGET_CONFIG_LOCATION}/provider.json
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function get_vmuuid() {
+    local VMNAME=$1
+    
+	openstack server show -f json "${VMNAME}" 2>/dev/null| jq -r '.id // ""'
+}
+
+#===========================================================================================================================================
+#
+#===========================================================================================================================================
+function get_net_type() {
+	local TYPE=$(openstack network show $1 -f json 2>/dev/stderr | jq -r '."router:external"')
+
+	if [ -n "${TYPE}" ]; then
+		if [ ${TYPE} == "true" ]; then
+			echo -n "public"
+		elif [ ${TYPE} == "false" ]; then
+			echo -n "private"
+		else
+			echo -n ""
+		fi
+	else
+		echo -n ""
+	fi
+}

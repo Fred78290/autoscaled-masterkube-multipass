01647528cff2121f73e31d4d49422887c126af38
Fred78290 <frederic.boltz@gmail.com>
2024-05-20 13:45:18 +0000

    WIP: kubeapps

--- bin/create-kubeapps.sh
+++ bin/create-kubeapps.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
-export ETC_DIR=${TARGET_DEPLOY_LOCATION}/external-dns
+export ETC_DIR=${TARGET_DEPLOY_LOCATION}/kubeapps
+
+### Clear AppRepository after delete ns
+# kubectl patch AppRepository/bitnami -p '{"metadata":{"finalizers":[]}}' --type=merge
 
 mkdir -p ${ETC_DIR}
 
@@ -10,15 +13,29 @@ kubectl --kubeconfig=${TARGET_CLUSTER_LOCATION}/config --dry-run=client -o yaml 
 	| tee ${ETC_DIR}/kubeapps.yaml | kubectl apply -f -
 
 echo "---" >> ${ETC_DIR}/kubeapps.yaml
-kubectl --kubeconfig=${TARGET_CLUSTER_LOCATION}/config --dry-run=client -o yaml create clusterrolebinding kubeapps-operator -n kubeapps --clusterrole=cluster-admin --serviceaccount=kubeapps:kubeapps-operator \
+kubectl --kubeconfig=${TARGET_CLUSTER_LOCATION}/config --dry-run=client -o yaml create clusterrolebinding kubeapps-operator -n kubeapps \
+  --clusterrole=cluster-admin --serviceaccount=kubeapps:kubeapps-operator \
 	| tee -a ${ETC_DIR}/kubeapps.yaml| kubectl apply -f -
 
 echo "---" >> ${ETC_DIR}/kubeapps.yaml
 kubectl --kubeconfig=${TARGET_CLUSTER_LOCATION}/config --dry-run=client -o yaml create serviceaccount kubeapps-operator -n kubeapps \
 	| tee -a ${ETC_DIR}/kubeapps.yaml | kubectl apply -f -
 
+echo "---" >> ${ETC_DIR}/kubeapps.yaml
+cat <<EOF | tee -a ${ETC_DIR}/kubeapps.yaml | kubectl apply -f -
+apiVersion: v1
+kind: Secret
+type: kubernetes.io/service-account-token
+metadata:
+  name: kubeapps-operator-token
+  namespace: kubeapps
+  annotations:
+    kubernetes.io/service-account.name: kubeapps-operator
+EOF
+
 cat > ${ETC_DIR}/values.yaml <<EOF
 ingress:
+  ingressClassName: nginx
   enabled: true
   hostname: ${NODEGROUP_NAME}-kubeapps.${DOMAIN_NAME}
   tls: true
@@ -27,7 +44,7 @@ ingress:
     cert-manager.io/cluster-issuer: cert-issuer-prod
     external-dns.alpha.kubernetes.io/register: 'true'
     external-dns.alpha.kubernetes.io/ttl: '600'
-    external-dns.alpha.kubernetes.io/target: ${EXTERNAL_DNS_TARGET}
+    external-dns.alpha.kubernetes.io/target: ${MASTERKUBE}.${DOMAIN_NAME}
     external-dns.alpha.kubernetes.io/hostname: ${NODEGROUP_NAME}-kubeapps.${DOMAIN_NAME}
 EOF
 
@@ -37,3 +54,7 @@ helm install kubeapps \
 	--set useHelm3=true \
 	--values ${ETC_DIR}/values.yaml \
 	bitnami/kubeapps
+
+echo_blue_dot_title "KubeApps token: "
+
+kubectl get --namespace kubeapps secret kubeapps-operator-token --kubeconfig=${TARGET_CLUSTER_LOCATION}/config -o go-template='{{.data.token | base64decode}}'

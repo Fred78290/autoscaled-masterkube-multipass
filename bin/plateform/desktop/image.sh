98159b656d5a5809b52ac56ddb73385164574409
Fred78290 <frederic.boltz@gmail.com>
2024-06-13 08:03:49 +0000

    FIX: Remove halt point

--- bin/plateform/desktop/image.sh
+++ bin/plateform/desktop/image.sh
@@ -254,7 +254,6 @@ if [ -z "${SEEDIMAGE_UUID}" ] || [ "${SEEDIMAGE_UUID}" == "ERROR" ]; then
 
 		if [ -z "${SEEDIMAGE_UUID}" ] || [ "${SEEDIMAGE_UUID}" == "ERROR" ]; then
 			echo_red_bold "Register ${SEED_IMAGE} failed!"
-			read -p "Continue..."
 			rm -rf "${VMFOLDER}"
 			exit -1
 		fi
